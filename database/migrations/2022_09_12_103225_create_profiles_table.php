24aeddee97d619195f72dcb8355acfb2458df929
Leeo17 <leeo.oneda@gmail.com>
2022-09-13 08:10:39 -0300

    Section 22

--- database/migrations/2022_09_12_103225_create_profiles_table.php
+++ database/migrations/2022_09_12_103225_create_profiles_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateBlogPostsTable extends Migration
+class CreateProfilesTable extends Migration
 {
   /**
    * Run the migrations.
@@ -13,11 +13,14 @@ class CreateBlogPostsTable extends Migration
    */
   public function up()
   {
-    Schema::create('blog_posts', function (Blueprint $table) {
+    Schema::create('profiles', function (Blueprint $table) {
       $table->id();
       $table->timestamps();
-      $table->string('title')->default('');
-      $table->text('content');
+
+      $table->foreignId('author_id')
+        ->unique()
+        ->constrained()
+        ->onDelete('cascade');
     });
   }
 
@@ -28,6 +31,6 @@ class CreateBlogPostsTable extends Migration
    */
   public function down()
   {
-    Schema::dropIfExists('blog_posts');
+    Schema::dropIfExists('profiles');
   }
 }
--- database/migrations/2022_09_12_123503_create_comments_table.php
+++ database/migrations/2022_09_12_123503_create_comments_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateBlogPostsTable extends Migration
+class CreateCommentsTable extends Migration
 {
   /**
    * Run the migrations.
@@ -13,11 +13,15 @@ class CreateBlogPostsTable extends Migration
    */
   public function up()
   {
-    Schema::create('blog_posts', function (Blueprint $table) {
+    Schema::create('comments', function (Blueprint $table) {
       $table->id();
       $table->timestamps();
-      $table->string('title')->default('');
+
       $table->text('content');
+
+      $table->foreignId('blog_post_id')
+        ->constrained()
+        ->onDelete('cascade');
     });
   }
 
@@ -28,6 +32,6 @@ class CreateBlogPostsTable extends Migration
    */
   public function down()
   {
-    Schema::dropIfExists('blog_posts');
+    Schema::dropIfExists('comments');
   }
 }
