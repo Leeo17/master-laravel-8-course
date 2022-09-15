ddd033fc3d94f0f85d487776e7331d5be765e17e
Leeo17 <leeo.oneda@gmail.com>
2022-09-15 10:55:50 -0300

    Section 24 - Database Seeding

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -6,13 +6,22 @@ use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     *
-     * @return void
-     */
-    public function run()
-    {
-        // \App\Models\User::factory(10)->create();
+  /**
+   * Seed the application's database.
+   *
+   * @return void
+   */
+  public function run()
+  {
+    if ($this->command->confirm('Do you want to refresh the database?')) {
+      $this->command->call('migrate:refresh');
+      $this->command->info('Database was refreshed');
     }
+
+    $this->call([
+      UsersTableSeeder::class,
+      BlogPostsTableSeeder::class,
+      CommentsTableSeeder::class
+    ]);
+  }
 }
