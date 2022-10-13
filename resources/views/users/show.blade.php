76f6f80fdf53ef9e79dc5998742a93018bc08667
Leeo17 <leeo.oneda@gmail.com>
2022-10-13 17:40:54 -0300

    Section 40 - Service, Service Container, Facades, Contracts

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -1,16 +1,18 @@
 @extends('layouts.app')
 @section('content')
-  <div class="row">
-    <div class="col-4">
-      <img src="{{ $user->image ? $user->image->url() : '' }}" class="img-thumbnail avatar" />
-    </div>
-    <div class="col-8">
-      <h3>{{ $user->name }}</h3>
-      @commentForm(['route' => route('users.comments.store', ['user' => $user->id])])
-      @endcommentForm
+    <div class="row">
+        <div class="col-4">
+            <img src="{{ $user->image ? $user->image->url() : '' }}" class="img-thumbnail avatar" />
+        </div>
+        <div class="col-8">
+            <h3>{{ $user->name }}</h3>
+            <p>Currently viewed by {{ $counter }} other users</p>
 
-      @commentList(['comments' => $user->commentsOn])
-      @endcommentList
+            @commentForm(['route' => route('users.comments.store', ['user' => $user->id])])
+            @endcommentForm
+
+            @commentList(['comments' => $user->commentsOn])
+            @endcommentList
+        </div>
     </div>
-  </div>
 @endsection
