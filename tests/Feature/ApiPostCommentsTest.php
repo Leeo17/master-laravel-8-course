6d811828bcdba196be67be63d162b88f2cad6df0
Leeo17 <leeo.oneda@gmail.com>
2022-10-26 11:39:55 -0300

    Section 44 - Testing APIs

---
+++ tests/Feature/ApiPostCommentsTest.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\BlogPost;
+use App\Models\Comment;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class ApiPostCommentsTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function testNewBlogPostDoesNotHaveComments()
+    {
+        // Arrange
+        $user = $this->user();
+
+        $this->blogPost();
+
+        // Act
+        $response = $this->actingAs($user)->json('GET', 'api/v1/posts/1/comments');
+
+        // Assert
+        $response->assertStatus(200)
+            ->assertJsonStructure(['data', 'links', 'meta'])
+            ->assertJsonCount(0, 'data');
+    }
+
+    public function testBlogPostHas10Comments()
+    {
+        // Arrange
+        $user = $this->user();
+
+        $this->blogPost()->each(function (BlogPost $post) {
+            $post->comments()->saveMany(
+                Comment::factory()->count(10)->make([
+                    'user_id' => $this->user()->id
+                ])
+            );
+        });
+
+        // Act
+        $response = $this->actingAs($user)->json('GET', 'api/v1/posts/2/comments');
+
+        // Assert
+        $response->assertStatus(200)
+            ->assertJsonStructure([
+                'data' => [
+                    '*' => [
+                        'id',
+                        'content',
+                        'created_at',
+                        'updated_at',
+                        'user' => [
+                            'id',
+                            'name'
+                        ]
+                    ]
+                ],
+                'links',
+                'meta'
+            ])
+            ->assertJsonCount(10, 'data');
+    }
+
+    public function testAddingCommentsWhenNotAuthenticated()
+    {
+        $this->blogPost();
+
+        $response = $this->json('POST', 'api/v1/posts/3/comments', [
+            'content' => 'Hello'
+        ]);
+
+        $response->assertUnauthorized();
+    }
+
+    public function testAddingCommentsWhenAuthenticated()
+    {
+        $this->blogPost();
+
+        $response = $this->actingAs($this->user())->json('POST', 'api/v1/posts/4/comments', [
+            'content' => 'Hello'
+        ]);
+
+        $response->assertCreated();
+    }
+
+    public function testAddingCommentWithInvalidData()
+    {
+        $this->blogPost();
+
+        $response = $this->actingAs($this->user())->json('POST', 'api/v1/posts/5/comments', []);
+
+        $response->assertStatus(422)
+            ->assertJson([
+                'message' => 'The given data was invalid.',
+                'errors' => [
+                    'content' => [
+                        'The content field is required.'
+                    ]
+                ]
+            ]);
+    }
+}
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,15 +2,23 @@
 
 namespace Tests;
 
+use App\Models\BlogPost;
 use App\Models\User;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
-  use CreatesApplication;
+    use CreatesApplication;
 
-  protected function user()
-  {
-    return User::factory()->create();
-  }
+    protected function user()
+    {
+        return User::factory()->create();
+    }
+
+    protected function blogPost()
+    {
+        return BlogPost::factory()->create([
+            'user_id' => $this->user()->id
+        ]);
+    }
 }
